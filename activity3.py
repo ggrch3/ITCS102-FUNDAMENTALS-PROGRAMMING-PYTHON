034b93687d5a524ac3b0d8edfcd90da5b82188fa
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-08-12 20:27:29 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,3 @@
+name = input("what is your name?" )
+print("WELCOME\n\t\tTO YOUR DOOM {},".format(name))
+print("\t\tyou should kys -Frisk\n \t\tcheers lads -Sans.")
---
+++ code_challange.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+print("\t\t\t\t*\n\n\n"
+      "\t\t\t*\t\t*\n\n\n"
+      "\t\t*\t\t\t\t*\n\n\n"
+      "\t*\t\t\t\t\t\t*\n\n\n"
+      "*\t\t\tHi\t\033[34m{}\033[0m\t\t\t\t*\n\n\n"
+      "\t*\t\t\t\t\t\t*\n\n\n"
+      "\t\t*\t\t\t\t*\n\n\n"
+      "\t\t\t*\t\t*\n\n\n"
+      "\t\t\t\t*".format(name))
