7cd4358476243b80e3fef60a30a446aa3b7a7a99
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-08-15 16:07:15 +0800

    Add files via upload

---
+++ code_challange2.py
@@ -0,0 +1,29 @@
+x = eval(input("please enter the sum you want to deposit :"))
+n1 = 1000
+n2 = 500
+n3 = 200
+n4 = 100
+n5 = 50
+n6 = 20
+n7 = 10
+n8 = 5
+n9 = 1
+
+print(x // n1, "-", n1)
+x = x % n1
+print(x // n2, "-", n2)
+x = x % n2
+print(x // n3, "-", n3)
+x = x % n3
+print(x // n4, "-", n4)
+x = x % n4
+print(x // n5, "-", n5)
+x = x % n5
+print(x // n6, "-", n6)
+x = x % n6
+print(x // n7, "-", n7)
+x = x % n7
+print(x // n8, "-", n8)
+x = x % n8
+print(x // n9, "-", n9)
+x = x % n9
