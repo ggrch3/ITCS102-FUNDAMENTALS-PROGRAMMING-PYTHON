b08aa709aeb270189fd85a2a357438339fdaeab8
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-08-27 19:37:57 +0800

    Add files via upload

---
+++ Code_prct.py
@@ -0,0 +1,5 @@
+print(" I  D  E  N  T  I  F  I  C  A  T  I  O  N        V  E  R  I  F  I  C  A  T  I  O  N      R  E  Q  U  I  R  E  D")
+
+name = input("Please enter your name --> ")
+IsStudent = input("Are you a student? (yes/no) ")
+
