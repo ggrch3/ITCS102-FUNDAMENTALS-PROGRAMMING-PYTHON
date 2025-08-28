e7e80eec5ccaadf099b3404eea5a60170c08166e
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-08-29 00:26:27 +0800

    Add files via upload

---
+++ code_challange3.py
@@ -0,0 +1,31 @@
+username = ("ggrch3")
+password = ("hakdog123")
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+import time
+
+if password == "hakdog123" and username == "ggrch3":
+    print("ACCESS GRANTED", end="", flush=True)
+    time.sleep(1)
+    print("\nWelcome")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print("you scum.", end="", flush=True)
+
+else:
+    print("You are not the person you used to be,", end="", flush=True)
+    time.sleep(1)
+    print("\n.")
+    time.sleep(1)
+    print("\n.")
+    time.sleep(1)
+    print("\n.")
+    time.sleep(1)
+    print("Scum.")
