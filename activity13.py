e50b90ebe7fc2b1b45dd0869c1bdc7c92c9af44b
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-09-13 23:43:45 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,11 @@
+#using for loop, ask to use input
+#after input, get the sumation of all the numner
+
+num = 17
+for X in range(1,11):
+    number = eval(input("Insert number: "))
+    print("Good boy")
+    num += number #add to num
+
+print("total:", num)
+
