af3e621bb60a8c493e9e5768e0332d472825a38f
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-09-23 21:32:29 +0800

    Add files via upload

---
+++ code_challange11.py
@@ -0,0 +1,15 @@
+
+
+for i in range(1, 11, 1):
+    for y in range(10, i, -1):
+        print("",end=" ")
+    for z in range(1, i, 1):
+        print("*", end= " ")
+    print() 
+
+for i in range(1, 11, 1):
+    for y in range(1, i, 1):
+        print(" ",end="")
+    for z in range(10, i, -1):
+        print("*", end= " ")
+    print() 
