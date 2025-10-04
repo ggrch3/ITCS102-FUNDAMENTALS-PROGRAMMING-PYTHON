878acc194c30d1002c6b002725dc13576f797a59
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-10-05 00:04:51 +0800

    code_challange12.py

--- code_challange12.py
+++ code_challange12.py
@@ -2,11 +2,13 @@
 
 
 
-for i in range(1, 8):
-    for x in range(8, i, -1):
+for w in range(1, 8):
+    for x in range(8, w, -1):
         print(" ", end=" ")
-    for y in range(i, 0, -1):
+    for y in range(w, 0, -1):
         print(y, end=" ")
-    for z in range(1, i):
-        print(i, end=" ")
+    for z in range(1, w):
+        print(w, end=" ")
+
+    
     print()
