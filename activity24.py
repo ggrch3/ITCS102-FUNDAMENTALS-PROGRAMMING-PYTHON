39595dae4fa8a6b0291d346465b92b1c5d55522d
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-11-12 01:18:15 +0800

    Add files via upload

---
+++ activity24.py
@@ -0,0 +1,16 @@
+
+name = 'Geroche'
+
+def greeter(name):
+    print(f"Hello {name}")
+    
+# greeter(name)
+# greeter(Leonardo)
+# greeter(Rafael)
+# greeter(Donatello)
+# greeter(MichaelAngelo)
+def totalmation(x):
+    total = 0
+    for y in range(x, 0, -1):
+        total += y
+    print(f"total of {x} is {total}")
