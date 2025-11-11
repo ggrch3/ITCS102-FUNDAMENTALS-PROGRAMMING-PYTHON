09df99755b0e6b6407d12b399af2da15cc992bb9
ggrch3 <gerochepaulumanzano7@gmail.com>
2025-11-12 01:17:31 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,35 @@
+
+
+months = ['jan','Feb','Mar','Apr','May','Jun','Jul']
+
+#functions
+months.append('Aug')
+print(months)
+months.append('Sept')
+
+
+#iteration / traversing a list
+for m in months:
+    print(f"{m}, 2025")
+
+
+fullname = 'Geroche Paul U. Manzano'
+
+# for i in fullname:
+#     print(i)
+
+
+
+# for i in fullname[::-1]:
+#     print(i)
+
+
+another = list(fullname)
+print(another)
+
+
+# fullname.reverse()
+# print(fullname)
+
+# months.reverse()
+# print(months)
